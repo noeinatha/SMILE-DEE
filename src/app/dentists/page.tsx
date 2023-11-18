0dc98cd3a112021d6c3ef7089d192f1615c7065a
noeinatha <92200039+noeinatha@users.noreply.github.com>
2023-11-18 15:31:32 +0700

    fetch dentists pages

--- src/app/dentists/page.tsx
+++ src/app/dentists/page.tsx
@@ -1,10 +1,12 @@
 import DentistCatalog from "@/components/DentistCatalog";
 import AdDentistCatalog from "@/components/AdDentistCatalog";
+import getDentists from "@/libs/getDentists";
 export default function Dentists() {
+  const dentists = getDentists();
   return (
     <div className=" h-screen w-full">
-      <AdDentistCatalog />
-      {/* <DentistCatalog/> */}
+      {/* <AdDentistCatalog/> */}
+      <DentistCatalog dentistJson={dentists} />
     </div>
   );
 }
--- src/components/AdDentistCatalog.tsx
+++ src/components/AdDentistCatalog.tsx
@@ -1,43 +1,58 @@
-"use client";
 import DentistCard from "@/components/DentistCard";
-import { Icon } from '@iconify/react';
+import { Icon } from "@iconify/react";
 import CreateDentistCard from "./CreateDentistCard";
 import { Fragment } from "react";
-import * as React from 'react';
+import * as React from "react";
 import { useState } from "react";
-export default function AdDentistCatalog(){
-    function addDentist(){
-        console.log("add dentist")
-    }
-    const [showModal, setShowModal] = useState(false);
-    const mockDentist = [{id : '01', dentistName: 'นายแพทย์พอใจ ฟันสวย', dentistExpertist: 'make kids cry', hospitalName: 'SW dev Hospity', hospitalAddress: 'some where near ur house', dentistTel: '012-345678', imgSrc: '/img/defaultProfile.jpg'},
-    {id : '02',dentistName: 'นายแพทย์พอใจ ฟันสวย', dentistExpertist: 'make kids cry', hospitalName: 'SW dev Hospity', hospitalAddress: 'some where near ur house', dentistTel: '012-345678', imgSrc: '/img/defaultProfile.jpg'},
-    {id : '03',dentistName: 'นายแพทย์พอใจ ฟันสวย', dentistExpertist: 'make kids cry', hospitalName: 'SW dev Hospity', hospitalAddress: 'some where near ur house', dentistTel: '012-345678', imgSrc: '/img/defaultProfile.jpg'},
-    {id : '04',dentistName: 'นายแพทย์พอใจ ฟันสวย', dentistExpertist: 'make kids cry', hospitalName: 'SW dev Hospity', hospitalAddress: 'some where near ur house', dentistTel: '012-345678', imgSrc: '/img/defaultProfile.jpg'},
-    {id : '05',dentistName: 'นายแพทย์พอใจ ฟันสวย', dentistExpertist: 'make kids cry', hospitalName: 'SW dev Hospity', hospitalAddress: 'some where near ur house', dentistTel: '012-345678', imgSrc: '/img/defaultProfile.jpg'}]
-    return (
-        <div>
-            <Fragment>
-            <div className= "flex flex-row justify-between items-center w-full px-20">
-                <div className="w-full text-5xl font-bold text-center text-red py-5 ">GET TO KNOW OUR  <span className="text-darkblue"> DENTIST</span> </div>
-                <button className='w-[45px] h-[45px] rounded-full relative bg-vividpurple flex justify-center items-center' onClick={() => setShowModal(true)}>
-                    <Icon icon="gridicons:create"  color="white" className= "w-3/5 h-3/5"/>
-                </button>
-                </div>
-            <div className=" m-5 flex flex-wrap justify-start gap-20 px-20">
-            {mockDentist.map((e) => (
-                    <DentistCard
-                        dentistName={e.dentistName}
-                        dentistExpertist={e.dentistExpertist}
-                        hospitalName={e.hospitalName}
-                        hospitalAddress={e.hospitalAddress}
-                        dentistTel={e.dentistTel}
-                        imgSrc={e.imgSrc}
-                    />
-                ))}
-            </div>
-            <CreateDentistCard isVisible = {showModal} onClose={() => {setShowModal(false)}}/>
-            </Fragment>
+export default async function AdDentistCatalog({
+  dentistJson,
+}: {
+  dentistJson: Object;
+}) {
+  const dentistJsonReady = await dentistJson;
+
+  function addDentist() {
+    console.log("add dentist");
+  }
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <div>
+      <Fragment>
+        <div className="flex flex-row justify-between items-center w-full px-20">
+          <div className="w-full text-5xl font-bold text-center text-red py-5 ">
+            GET TO KNOW OUR <span className="text-darkblue"> DENTIST</span>{" "}
+          </div>
+          <button
+            className="w-[45px] h-[45px] rounded-full relative bg-vividpurple flex justify-center items-center"
+            onClick={() => setShowModal(true)}
+          >
+            <Icon
+              icon="gridicons:create"
+              color="white"
+              className="w-3/5 h-3/5"
+            />
+          </button>
         </div>
-        );
+        <div className=" m-5 flex flex-wrap justify-start gap-20 px-20">
+          {dentistJsonReady.data.map((dentistItem: Object) => (
+            <DentistCard
+              dentistName={dentistItem.name}
+              dentistExpertist={dentistItem.expertist}
+              hospitalName={dentistItem.hospital}
+              hospitalAddress={dentistItem.address}
+              dentistTel={dentistItem.tel}
+              imgSrc={dentistItem.picture}
+            />
+          ))}
+        </div>
+        <CreateDentistCard
+          isVisible={showModal}
+          onClose={() => {
+            setShowModal(false);
+          }}
+        />
+      </Fragment>
+    </div>
+  );
 }
