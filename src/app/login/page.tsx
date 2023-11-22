a73d08c3ecb6357ffb86448301de5e5c3c1a57dc
noeinatha <92200039+noeinatha@users.noreply.github.com>
2023-11-22 11:27:33 +0700

    login bg

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,9 @@
 import { LoginForm } from "@/components/LoginForm";
 
-
 export default function LoginPage() {
   return (
-    <div className= "flex min-h-screen flex-col items-center justify-between p-14 top bg-gradient-to-l from-darkpurple to-fadebg">
-        <LoginForm/>
+    <div className="h-[calc(100vh-80px)] flex flex-col items-center justify-between p-14 top bg-gradient-to-l from-darkpurple to-fadebg">
+      <LoginForm />
     </div>
- 
   );
 }
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,9 +1,8 @@
 import RegisterForm from "@/components/RegisterForm";
 export default function RegisterPage() {
   return (
-    <div className= "flex min-h-screen flex-col items-center justify-between p-14 top bg-gradient-to-r from-darkpurple to-fadebg">
-        <RegisterForm/>
+    <div className="flex h-[calc(100vh-80px)] flex-col items-center justify-between p-14 top bg-gradient-to-r from-darkpurple to-fadebg">
+      <RegisterForm />
     </div>
- 
   );
 }
